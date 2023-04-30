e35d7004922374c1545a85ecf53ef839556ee499
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-01 00:30:12 +0200

    Created hashingSystem.cpp

---
+++ hashingSystem/hashingSystem.cpp
@@ -0,0 +1,47 @@
+#include <math.h>
+#include <string>
+
+namespace hashing {
+    
+    unsigned int md5() {
+        
+        
+        
+    };
+    unsigned int scrypt() {
+        
+        
+        
+    };
+    unsigned int sha256(string input) {
+        
+        
+        
+    };
+    string sha512() {
+        
+        
+        
+    };
+    unsigned int x11() {
+        
+        
+        
+    };
+    unsigned int x12() {
+        
+        
+        
+    };
+    unsigned int x16r() {
+        
+        
+        
+    };
+    unsigned int x16s() {
+        
+        
+        
+    };
+    
+}
