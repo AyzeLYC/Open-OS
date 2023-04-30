3ce52c859a2c7760f1b54099247807ca01e7d299
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-01 00:12:02 +0200

    Updated os.cpp

--- os.cpp
+++ os.cpp
@@ -1,15 +1,37 @@
 #include <string>
 
+#include "audioSystem/audioSystem.cpp"
+#include "commandlineSystem/commandlineSystem.cpp"
+#include "fileSystem/fileSystem.cpp"
+#include "interfaceSystem/interfaceSystem.cpp"
+#include "threadingSystem/threadingSystem.cpp"
+#include "userSystem/userSystem.cpp"
+
 namespace os {
     
     unsigned int start() {
         
+        string hddDatas,
+               osDatas,
+               usersDatas[],
+               programsDatas[];
         
+        fs.start();
+        threading.start();
+        user.start();
+        cli.start();
+        interface.start();
+        audio.start();
         
     };
     unsigned int stop() {
         
-        
+        audio.stop();
+        interface.stop();
+        cli.stop();
+        user.stop();
+        threading.stop();
+        fs.stop();
         
     };
     
