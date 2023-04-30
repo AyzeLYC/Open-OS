d45a3dc9001682612789b08341d6066089c7d7fa
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-04-30 12:48:11 +0200

    Created userSystem.cpp

---
+++ userSystem.cpp
@@ -0,0 +1,30 @@
+#include <string>
+
+#include "../fileSystem/fileSystem.cpp"
+
+namespace user {
+    
+    string CurrentUserid;
+    
+    unsigned int GetCurrentUserId() {
+        
+        return userid;
+        
+    };
+    unsigned int GetUserName(string UserId) {
+        
+        fstream USER;
+        USER.open("C:\Users\users.datas", fstream::in);
+        if (USER.length() >= stoull(UserId)) {
+            
+            return USER[to_string(UserId)] && USER.close();
+            
+        } else {
+            
+            return "error";
+            
+        };
+        
+    };
+    
+}
