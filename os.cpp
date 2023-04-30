bafb1aa7a999b704fa0add253cfbe24233847d94
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-01 00:34:27 +0200

    Updated os.cpp

--- os.cpp
+++ os.cpp
@@ -2,19 +2,22 @@
 
 #include "audioSystem/audioSystem.cpp"
 #include "commandlineSystem/commandlineSystem.cpp"
+#include "decryptionSystem/decryptionSystem.cpp"
+#include "encryptionSystem/encryptionSystem.cpp"
 #include "fileSystem/fileSystem.cpp"
+#include "hashingSystem/hashingSystem.cpp"
 #include "interfaceSystem/interfaceSystem.cpp"
 #include "threadingSystem/threadingSystem.cpp"
 #include "userSystem/userSystem.cpp"
 
 namespace os {
     
+    string hddFilesDatas,
+           osFilesDatas,
+           usersFilesDatas[],
+           programsFilesDatas[];
+    
     unsigned int start() {
-        
-        string hddDatas,
-               osDatas,
-               usersDatas[],
-               programsDatas[];
         
         fs.start();
         threading.start();
