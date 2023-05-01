56787780fb12eefd566eaf583d972f3e28f7ca35
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-01 20:03:57 +0200

    Updated userSystem.cpp

--- userSystem/userSystem.cpp
+++ userSystem/userSystem.cpp
@@ -4,7 +4,21 @@
 
 namespace user {
     
+    struct UserDatas {
+        
+        string username;
+        uint1_t userRights; // 0 = non admin ( can create its own files, folders and modify its parameters but can't modify the other users files, folder and parameters ), 1 = admin ( can create its own files, folderd and modify its parameters and create some filders, folders and modoify the other users parameters )
+        string files[];
+        
+    }
+    
     string CurrentUserid;
+    
+    unsigned int parameters(string UserId) {
+        
+        
+        
+    };
     
     unsigned int GetCurrentUserId() {
         
