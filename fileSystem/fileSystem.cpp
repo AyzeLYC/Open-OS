e2c8efe2a88d7c9f65b0e1702bdd0c2cec687b04
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-02 11:19:00 +0200

    Updated fileSystem.cpp

--- fileSystem/fileSystem.cpp
+++ fileSystem/fileSystem.cpp
@@ -2,6 +2,80 @@
 
 namespace fs {
     
+    struct pngFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    struct jpgFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    struct jpegFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    struct webpFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    
+    struct gifFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    
+    struct mp4FileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    struct webmFileDatas {
+        
+        struct metadatas {
+            
+            string name;
+            
+        }
+        string datas;
+        
+    }
+    
+    
     unsigned int createFile() {
         
         
