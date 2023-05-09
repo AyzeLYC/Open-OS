e6a83dc1406398ae246c04d293af50edbe02c782
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-09 10:31:26 +0200

    Updated internetSystem.cpp

--- internetSystem/internetSystem.cpp
+++ internetSystem/internetSystem.cpp
@@ -5,12 +5,46 @@
 
 namespace internet {
     
-    unsigned int socket() {
+    struct socketAddress {
+        
+        string address;
+        uint1_t port;
+        
+    }
+    
+    struct socketDatas {
+        
+        int domain,
+        string type;
+        uint1_t protocol;
+        
+    }
+    struct socketBindDatas {
         
         
+        
+    }
+    
+    map<string, socketDatas> sockets;
+    
+    unsigned int socket(int domain, string type, uint8_t protocol) {
+        
+        if (type == "SOCK_STREAM") {
+            
+            
+            
+        } else if (type == "SOCK_DGRAM") {
+            
+            
+            
+        } else if (type == "UNIX") {
+            
+            
+            
+        };
         
     };
-    unsigned int bind() {
+    unsigned int bind(int sockfd, const socketAddress *address, uint8_t addrlen) {
         
         
         
@@ -20,12 +54,12 @@ namespace internet {
         
         
     };
-    unsigned int listen() {
+    unsigned int listen(int sockfd, int backlog) {
         
         
         
     };
-    unsigned int accept() {
+    unsigned int accept(socketDatas SOCKET, socketAddress *addr, uint8_t addrlen) {
         
         
         
