72bcfc93395831d42415acde08ec63aeb7b5f45f
AyzeLYC <35877385+AyzeLYC@users.noreply.github.com>
2023-05-15 09:48:14 +0200

    Updated interfaceSystem.cpp

--- interfaceSystem/interfaceSystem.cpp
+++ interfaceSystem/interfaceSystem.cpp
@@ -2,6 +2,40 @@
 
 namespace interface {
     
+    string apps[];
     
+    unsigned long long system() {
+        
+        unsigned int start() {
+            
+            
+            
+        };
+        unsigned int pause() {
+            
+            
+            
+        };
+        unsigned int stop() {
+            
+            
+            
+        };
+        
+    };
+    unsigned long long application() {
+        
+        unsigned int create(uint1_t type) { // if type == 0 then the app is a command line based app, if type == 1 then the app is a user interface based app
+            
+            
+            
+        };
+        unsigned int modify(uint2_t appPart) {
+            
+            
+            
+        };
+        
+    };
     
 }
